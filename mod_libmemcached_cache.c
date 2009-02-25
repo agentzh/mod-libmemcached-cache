3d1a03c5388be8e82e47ab2802928e2f3678948a
agentzh <agentzh@gmail.com>
2009-02-25 10:36:16 +0800

    added copyright notice

--- mod_libmemcached_cache.c
+++ mod_libmemcached_cache.c
@@ -1,3 +1,23 @@
+/* mod_libmemcached_cache.c - libmemcached storage backend for mod_cache */
+/*
+ *    Copyright (C) 2009 Yahoo! China EEEE Works, Alibaba Inc.
+ *
+ *    This program is free software; you can redistribute it and/or modify it
+ *    under the terms of the GNU General Public License as published by the
+ *    Free Software Foundation; either version 2, or (at your option) any
+ *    later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
+
+/* #define AP_DEBUG */
+
 #include "apr_strings.h"
 #include "util_filter.h"
 #include "util_script.h"
